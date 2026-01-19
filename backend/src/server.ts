e1f78ae5c97d77ae87dc52b6ce05b2ab904ffcca
Sonuraj804456 <ksonu05104@gmail.com>
2026-01-19 21:50:20 +0530

    updated backend with TypeScript setup

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from "express"
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Backend running with TypeScript 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
