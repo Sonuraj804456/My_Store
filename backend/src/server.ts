49a04c6891b335eb2994c056adfc6479eb3d2d45
Sonuraj804456 <ksonu05104@gmail.com>
2026-01-20 10:26:16 +0530

    Update backend code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Backend running with TypeScript 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
